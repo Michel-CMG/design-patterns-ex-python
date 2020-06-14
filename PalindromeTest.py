6b8f96be5f7daf787b3c1782684f5904b952b0d3
Michel-CMG <64927362+Michel-CMG@users.noreply.github.com>
2020-06-14 16:27:09 +0900

    PalindromeTest: test python input(keyboard) output(console). use python3 for directly input message other than 'input message'. set encoding to utf8.

---
+++ PalindromeTest.py
@@ -0,0 +1,15 @@
+# This Python file uses the following encoding: utf-8
+
+# Use python3 to run the code.
+
+def reverse(text):
+  return text[::-1]
+
+def is_palindrome(text):
+  return text == reverse(text)
+
+something = input("Enter text: ")
+if is_palindrome(something):
+  print("Yes, it is a palindrome")
+else:
+  print("No, it is not a palindrome")
